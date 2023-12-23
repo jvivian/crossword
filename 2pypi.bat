31f090dbb631b556193e22c1882a4a5267e92be5
John Vivian <jtvivian@gmail.com>
2023-12-23 15:57:51 -0800

    Publish crossword

--- 2pypi.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd "%~dp0"
-
-echo ">>>>>> Uploading to PyPi..."
-python -m twine upload --verbose -u s0mbre dist\*
-
-echo ">>>>>> FINISHED!"
-set /p=Press ENTER...
--- 2testpypi.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd "%~dp0"
-
-echo ">>>>>> Uploading to TestPyPi..."
-python -m twine upload --repository testpypi -u s0mbre dist\*
-
-echo ">>>>>> FINISHED!"
-set /p=Press ENTER...
